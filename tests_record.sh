2fe99f6f73d2c9ffda045b56a04c8e1cef3dd6b0
Julien <julien@zenaton.com>
2019-08-26 10:36:48 +0200

    in progress

--- tests_record.sh
+++ tests_record.sh
@@ -11,16 +11,5 @@ for filename in ./bin/*; do
   sleep 30
   cp zenaton.out ./test/output/${basename}.out
   cp zenaton.err ./test/output/${basename}.err
-#  if cmp --silent ./zenaton.out ./tests/$($basename)_expected_output ; then
-#    echo "Command succeeded"
-#    exit 0
-#  else
-#    echo "Tests failed"
-#    echo "Expected:"
-#    cat ./tests/$($basename)_expected_output
-#    echo "Got:"
-#    cat ./zenaton.out
-#    exit 1
-#  fi
 done
 
