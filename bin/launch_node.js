23317622291ebd76189c27938573fed3d1cc7c70
Julien <julien@zenaton.com>
2019-09-05 11:54:01 +0200

    in progress

--- bin/launch_node.js
+++ bin/launch_node.js
@@ -1,4 +1,7 @@
-require("../client");
+// require("../client");
+
+const argv = require("minimist")(process.argv.slice(2));
+const record = argv._.includes("record") ? true : false;
 
 const NodeWorkflow = require("../src/Workflows/NodeWorkflow");
 
--- src/Launchers/Node/Demo.js
+++ src/Launchers/Node/Demo.js
@@ -1,9 +1,12 @@
 const { Task } = require("zenaton");
 const DemoWorkflow = require("../../Tests/Node/DemoWorkflow");
+const fs = require("fs");
 
 module.exports = Task("LaunchDemo", {
   async handle() {
     let response;
+    let contents = fs.readFileSync("DATA", "utf8");
+
     await new DemoWorkflow().dispatch();
 
     await new Promise(resolve => {
